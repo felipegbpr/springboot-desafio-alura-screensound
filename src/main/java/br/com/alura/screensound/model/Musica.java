1a638b1c1601f0da7f97617296de17fa9d5fc431
Felipe Borges <felipetecacc@gmail.com>
2024-06-12 18:41:30 -0300

    Salvando músicas e artistas no banco de dados

--- src/main/java/br/com/alura/screensound/model/Musica.java
+++ src/main/java/br/com/alura/screensound/model/Musica.java
@@ -14,6 +14,12 @@ public class Musica {
     @ManyToOne
     private Artista artista;
 
+    public Musica() {}
+
+    public Musica(String nomeMusica) {
+        this.titulo = nomeMusica;
+    }
+
     public Long getId() {
         return id;
     }
@@ -41,6 +47,6 @@ public class Musica {
     @Override
     public String toString() {
         return "titulo='" + titulo + '\'' +
-                ", artista=" + artista;
+                ", artista=" + artista.getNome();
     }
 }
